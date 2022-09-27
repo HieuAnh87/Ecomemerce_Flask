5ead47fbed7db7c84d4e93a9b79b41395e91cb3e
Hieu Anh-Mac <dhieu23nd@gmail.com>
2022-09-27 14:05:02 +0700

    Add: add Addproduct model and test db

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from src import db
 
 
@@ -9,6 +11,30 @@ class Brand(db.Model):
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False, unique=True)
+
+
+class Addproduct(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    discount = db.Column(db.Integer, default=0)
+    stock = db.Column(db.Integer, nullable=False)
+    colors = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Numeric(10, 2), nullable=False)
+    desc = db.Column(db.Text, nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    category = db.relationship('Category', backref=db.backref('categories', lazy=True))
+
+    brand_id = db.Column(db.Integer, db.ForeignKey('brand.id'), nullable=False)
+    brand = db.relationship('Brand', backref=db.backref('brands', lazy=True))
+
+    image_1 = db.Column(db.String(150), nullable=False, default='image1.jpg')
+    image_2 = db.Column(db.String(150), nullable=False, default='image2.jpg')
+    image_3 = db.Column(db.String(150), nullable=False, default='image3.jpg')
+
+    def __repr__(self):
+        return '<Post %r>' % self.name
 
 
 db.create_all()
